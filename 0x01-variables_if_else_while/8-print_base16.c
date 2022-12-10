e98df53527a7407331119028ac44f2c841bfe985
olajumoke4521 <haonatararomi@gmail.com>
2022-12-10 09:21:32 -0800

    numbers in base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - print the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int num ='0';
+	int num = '0';
 	char letter = 'a';
 
 	while (num <= '9')
