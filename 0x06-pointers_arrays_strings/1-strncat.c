b9250564a8d5558a481efd71fa94e71ce8198ff1
olajumoke4521 <haonatararomi@gmail.com>
2022-12-22 17:39:30 -0800

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		length++;
 	}
-	for (j = 0; j < n && src[j] != '\0'; j++; length++)
+	for (j = 0; j < n && src[j] != '\0'; j++, length++)
 	{
 		dest[length] = src[j];
 	}
